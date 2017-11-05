e12fe3bda0a0de49df42d5a8d3155e6a43c66560
quasarmipt <justfastmeow@gmail.com>
2017-11-05 04:30:55 +0300

    added ParseExpression function

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <cassert>
+#include <stack>
 #define varepsilon 'e'
 
 // 0 подразумевается initial state всегда
@@ -392,9 +393,58 @@ namespace MyTests {
     }
 }
 
+CAutomata ParseExpression (std::string& expression) {
+    // Стек для хранения промежуточных автоматов, создаваемых по мере вычитывания регулярки
+    std::stack<CAutomata> AutomataStack;
+    for (char symbol : expression) {
+        switch (symbol) {
+            case 'a': case 'b': case 'c': {
+                AutomataStack.push(CAutomata(symbol));
+                break;
+            }
+            case '+': case '.': {
+                if (AutomataStack.size() >= 2) {
+                    CAutomata temp1 = AutomataStack.top();
+                    AutomataStack.pop();
+                    CAutomata temp2 = AutomataStack.top();
+                    AutomataStack.pop();
+                    AutomataStack.push(CAutomata(&temp1,&temp2,symbol));
+                } else {
+                    std::cout << "ERROR";
+                    exit(1);
+                }
+                break;
+            }
+            case '*': {
+                if (!AutomataStack.empty()) {
+                    CAutomata temp = AutomataStack.top();
+                    AutomataStack.pop();
+                    AutomataStack.push(CAutomata(&temp,symbol));
+                } else {
+                    std::cout << "ERROR";
+                    exit(2);
+                }
+                break;
+            }
+            default: {
+                std::cout << "ERROR";
+                exit(3);
+            }
+        }
+    }
+    if (AutomataStack.size() == 1) {
+        return AutomataStack.top();
+    } else {
+        std::cout << "ERROR";
+        exit(4);
+    }
+}
+
 int main() {
 
-    MyTests::runTests();
+    std::string some_expression_in_rpn;
+    std::cin >> some_expression_in_rpn;
+    CAutomata automata_from_expression = ParseExpression(some_expression_in_rpn);
+    automata_from_expression.PrintAutomata();
 
-    return 0;
 }
